7c0c05c5acdc69384f2c52b500c50c2e0b147ffb
HelMelon <helgamelon@yandex.ru>
2023-11-20 15:44:00 +0300

    Update repository
    Update repository

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import Header from "./components/Header.vue";
+import {onMounted } from 'vue';
+import getTheme from "./utils/themesSwitcher.js";
+onMounted(() => {
+  document.getElementById('app').classList.add(getTheme());
+});
+</script>
+
+<template>
+  <Header />
+  <RouterView />
+</template>
